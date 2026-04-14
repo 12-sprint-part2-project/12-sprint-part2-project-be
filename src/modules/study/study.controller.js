4b4c9240a879b8a3c2e5610d82e860e4933419a7
narin116 <aasdff1010@gmail.com>
2026-04-14 11:29:56 +0900

    feat: 스터디 생성 API 작성 완료 (진짜)

--- src/modules/study/study.controller.js
+++ src/modules/study/study.controller.js
@@ -8,6 +8,15 @@ export const getStudies = asyncHandler(async (req, res) => {
 
 export const createStudy = asyncHandler(async (req, res) => {
   const { nickname, title, description, theme, password } = req.body;
+
+  //에러 핸들링 (validation)
+  if (!title || !theme || !password || !nickname) {
+    //TODO:  description항목은 사용자가 작성하지 않아도 되는거라..얘 기본값 주는 걸 어디서 할지 의논이 필요할 것 같기도? 아니면 그냥 일단 null값으로넣어야 할까. 그래야겠다.
+    throw new BadRequestError(
+      "스터디 생성에 실패했습니다. 닉네임, 제목, 테마, 비밀번호는 필수입니다.",
+    ); //원래는 앞 문장만 정해뒀었는데 혹시모르니 뒤에 문장도 적어둠..
+    //이게 400에러를 반환하는데, 500에러는 알아서 처리되는 거겠지?
+  }
   const createdStudy = await studyService.createStudy({
     nickname,
     title,
@@ -27,14 +36,6 @@ export const updateStudy = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const { title, description, theme, password, nickname } = req.body;
 
-  //에러 핸들링 (validation)
-  if (!title || !theme || !password || !nickname) {
-    //TODO:  description항목은 사용자가 작성하지 않아도 되는거라..얘 기본값 주는 걸 어디서 할지 의논이 필요할 것 같기도? 아니면 그냥 일단 null값으로넣어야 할까. 그래야겠다.
-    throw new BadRequestError(
-      "스터디 생성에 실패했습니다. 닉네임, 제목, 테마, 비밀번호는 필수입니다.",
-    ); //원래는 앞 문장만 정해뒀었는데 혹시모르니 뒤에 문장도 적어둠..
-    //이게 400에러를 반환하는데, 500에러는 알아서 처리되는 거겠지?
-  }
   const updatedStudy = await studyService.updateStudy(id, {
     title,
     description,
