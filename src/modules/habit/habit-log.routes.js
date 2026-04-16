f8860c9e0d7bbb72f44bb5d7c63293149b6cceec
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-16 16:27:36 +0900

    refactor: study 하위 라우터에 studyExist 미들웨어 적용

--- src/modules/habit/habit-log.routes.js
+++ src/modules/habit/habit-log.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { validateId } from "../../common/middlewares/validateId.js";
+import { checkStudyExists } from "../../common/middlewares/studyExist.js";
 import * as habitController from "./habit.controller.js";
 
 const router = express.Router({ mergeParams: true });
 
 router.param("studyId", validateId);
+router.param("habitId", validateId);
+
+router.use(checkStudyExists);
 
 router.get("/", habitController.getWeeklyLogs); // ?date=YYYY-MM-DD
 
