be93e52d394725a353876cb854eb1feb43cf0c4e
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-16 13:36:53 +0900

    refactor: study/habit/habit-log/focus 라우터에 router.param() 등록

--- src/modules/focus/focus.routes.js
+++ src/modules/focus/focus.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { validateId } from "../middlewares/validateId.js";
 import * as focusController from "./focus.controller.js";
 
 const router = express.Router({ mergeParams: true });
+
+router.param("studyId", validateId);
+router.param("sessionId", validateId);
 
 router.get("/", focusController.getSession);
 router.post("/", focusController.createSession);
