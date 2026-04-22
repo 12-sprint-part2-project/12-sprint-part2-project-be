05ff13eba409e0f0156417f4ecd9906c809f598c
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-22 11:32:47 +0900

    fix: Promise.all 대신 쿼리문 in 으로 다중 호출 보완

--- src/modules/study/study.service.js
+++ src/modules/study/study.service.js
@@ -112,32 +112,31 @@ export const getStudies = async (data) => {
 };
 
 export const getRecentStudies = async (ids) => {
-  const res = await Promise.all(
-    ids.map(async (id) => {
-      const study = await prisma.study.findUniqueOrThrow({
-        where: { id },
-        omit: { password: true },
-        include: {
-          focusSessions: {
-            where: { status: "completed" },
-            select: {
-              earnedPoint: true,
-            },
-          },
-          emojis: true,
+  const studies = await prisma.study.findMany({
+    where: { id: { in: ids } },
+    omit: { password: true },
+    include: {
+      focusSessions: {
+        where: { status: "completed" },
+        select: {
+          earnedPoint: true,
         },
-      });
+      },
+      emojis: true,
+    },
+  });
 
-      const emojis = countedEmojis(study.emojis, 3);
+  const sorted = ids.map((id) => studies.find((study) => study.id === id));
 
-      const points = study.focusSessions.reduce(
-        (sum, s) => sum + s.earnedPoint,
-        0,
-      );
+  const res = sorted.map((study) => {
+    const emojis = countedEmojis(study.emojis, 3);
+    const points = study.focusSessions.reduce(
+      (sum, s) => sum + s.earnedPoint,
+      0,
+    );
 
-      return { ...study, points, emojis };
-    }),
-  );
+    return { ...study, points, emojis };
+  });
 
   return res;
 };
