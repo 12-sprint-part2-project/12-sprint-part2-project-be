60a3bff11d81fc08b8795c215b943ee34adaf47d
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-25 13:31:42 +0900

    refactor: asyncHandler에서 P2025 번역 로직 제거

--- src/common/middlewares/asyncHandler.js
+++ src/common/middlewares/asyncHandler.js
@@ -1,32 +1,7 @@
-import {
-  StudyNotFoundError,
-  HabitNotFoundError,
-  HabitLogNotFoundError,
-  FocusNotFoundError,
-} from "../../errors/CustomError.js";
-
-const PRISMA_P2025_MAP = {
-  study: StudyNotFoundError,
-  habit: HabitNotFoundError,
-  habitlog: HabitLogNotFoundError,
-  focussession: FocusNotFoundError,
-};
-
-const normalize = (name = "") => name.toLowerCase();
-
 const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    if (err.code === "P2025") {
-      const modelName = normalize(err.meta?.modelName);
-      // meta: { modelName: 'Study', operation: 'a query' },
-
-      const ErrorClass = PRISMA_P2025_MAP[modelName];
-
-      return next(ErrorClass ? new ErrorClass() : err);
-    }
-
     next(err);
   }
 };
